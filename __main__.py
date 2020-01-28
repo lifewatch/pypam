e31e6763bf28059745f0e1764a097b7bb5ab7aca
Clea Parcerisas <cleap@vliz.be>
2020-01-28 11:52:20 +0100

    Bugs with Vpp solved

--- __main__.py
+++ __main__.py
@@ -4,12 +4,13 @@ import acoustic_survey
 
 
 # Sound Analysis
-# folder_path = '//fs/shared/transfert/MRC/PAM 20190719'
-folder_path = "C:/Users/cleap/Documents/Data/Sound Data/Seiche/AutonautTest"
+folder_path = '//fs/shared/transfert/MRC/PAM 20190719'
+# folder_path = "C:/Users/cleap/Documents/Data/Sound Data/Seiche/AutonautTest"
 
 # Hydrophone Setup
 sensitiviy = -196.0
 preamp_gain = 0
+Vpp = 2.0               # If Vpp is 2.0 then it means the wav is -1 to 1 directly related to V
 model = 'uPam'
 name = 'Seiche'
 
@@ -21,10 +22,8 @@ if __name__ == "__main__":
     """
     Run 
     """
-    hydrophone = pyhydrophone.seiche.Seiche(name, model, sensitiviy, preamp_gain)
+    hydrophone = pyhydrophone.seiche.Seiche(name, model, sensitiviy, preamp_gain, Vpp)
     asa = acoustic_survey.ASA(folder_path=folder_path, hydrophone=hydrophone, ref_pressure=REF_PRESSURE)
-    # spl = asa.octavebands_spl_evolution()
     # fdistr = asa.freq_distr_evolution(verbose=True)
-    ldistr = asa.level_distr_evolution(verbose=True)
-
-    print(fdistr)
+    # ldistr = asa.level_distr_evolution(verbose=True)
+    fdistr, ldistr = asa.total_analysis(verbose=True)
