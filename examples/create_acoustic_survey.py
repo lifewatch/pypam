2893b0137a2d6b90ae604bfe4dee3135ebd5e784
Clea Parcerisas <clea.parcerisas@vliz.be>
2022-01-14 16:02:04 +0100

    Updated examples

--- examples/create_acoustic_survey.py
+++ examples/create_acoustic_survey.py
@@ -30,7 +30,7 @@ detection_band = [500, 1000]
 
 
 if __name__ == "__main__":
-    asa = pypam.ASA(hydrophone=soundtrap, folder_path='./../tests/test_data', binsize=binsize, nfft=nfft, utc=True,
-                    include_dirs=include_dirs, zipped=zipped_files, dc_subtract=dc_subtract)
+    asa = pypam.ASA(hydrophone=soundtrap, folder_path='./../tests/test_data', binsize=binsize, nfft=nfft,
+                    timezone='UTC', include_dirs=include_dirs, zipped=zipped_files, dc_subtract=dc_subtract)
     features_ds = asa.evolution_multiple(method_list=features, band_list=band_list)
     oct_ds = asa.evolution_freq_dom('third_octaves_levels', band=third_octaves, db=True)
--- examples/create_dataset.py
+++ examples/create_dataset.py
@@ -3,8 +3,10 @@ import pathlib
 import pyhydrophone as pyhy
 import pypam
 
+
 # Acoustic Data
-summary_path = pathlib.Path('./../tests/test_data/data_summary.csv')
+summary_path = pathlib.Path('./data_summary_example.csv')
+include_dirs = False
 
 # Output folder
 output_folder = summary_path.parent.joinpath('data_exploration')
@@ -18,8 +20,10 @@ soundtrap = pyhy.soundtrap.SoundTrap(name=name, model=model, serial_number=seria
 
 bk_model = 'Nexus'
 bk_name = 'B&K'
-amplif0 = 10e-3
-bk = pyhy.BruelKjaer(name=bk_name, model=bk_model, amplif=amplif0, serial_number=1)
+preamp_gain = -170
+bk_Vpp = 2.0
+bk = pyhy.BruelKjaer(name=bk_name, model=bk_model, preamp_gain=preamp_gain, Vpp=bk_Vpp, serial_number=1,
+                     type_signal='ref')
 
 upam_model = 'uPam'
 upam_name = 'Seiche'
@@ -27,8 +31,8 @@ upam_serial_number = 'SM7213'
 upam_sensitivity = -196.0
 upam_preamp_gain = 0.0
 upam_Vpp = 20.0
-upam = pyhy.Seiche(name=upam_name, model=upam_name, serial_number=upam_serial_number, sensitivity=upam_sensitivity,
-                   preamp_gain=upam_preamp_gain, Vpp=upam_Vpp)
+upam = pyhy.uPam(name=upam_name, model=upam_name, serial_number=upam_serial_number, sensitivity=upam_sensitivity,
+                 preamp_gain=upam_preamp_gain, Vpp=upam_Vpp)
 
 
 instruments = {'SoundTrap': soundtrap, 'uPam': upam, 'B&K': bk}
@@ -39,16 +43,21 @@ REF_PRESSURE = 1e-6
 # SURVEY PARAMETERS
 nfft = 4096
 binsize = 60.0
+overlap = 0.5
+dc_subtract = False
 band_lf = [50, 500]
 band_mf = [500, 2000]
 band_hf = [2000, 20000]
 band_list = [band_lf]
-features = ['rms', 'sel', 'aci']
-third_octaves = None
+temporal_features = ['rms', 'sel', 'aci']
+frequency_features = ['third_octaves_levels']
+
+n_join_bins = 3
 
 if __name__ == "__main__":
     # Create the dataset object
-    ds = pypam.DataSet(summary_path, output_folder, instruments, features, third_octaves, band_list, binsize,
-                       nfft)
+    ds = pypam.dataset.DataSet(summary_path, output_folder, instruments, temporal_features=temporal_features,
+                               frequency_features=frequency_features, bands_list=band_list, binsize=binsize,
+                               nfft=nfft, overlap=overlap, dc_subtract=dc_subtract, n_join_bins=n_join_bins)
     # Call the dataset creation. Will create the files in the corresponding folder
     ds()
