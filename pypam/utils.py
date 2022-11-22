20f31592c1ce985dbb418d2a181ddab32a0e7aac
Clea Parcerisas <cleaparcerisas@gmail.com>
2022-11-22 22:39:47 +0100

    added millidecade bands, added tests for utils module and files for testing the millidecades according to MANTA

--- pypam/utils.py
+++ pypam/utils.py
@@ -268,6 +268,194 @@ def octbankdsgn(fs, bands, fraction=1, n=2):
     return filterbank, fsnew, d
 
 
+def get_bands_limits(band, nfft, base, bands_per_division, hybrid_mode, first_bin_centre=0):
+    """
+
+    Parameters
+    ----------
+    band
+    nfft
+    base
+    bands_per_division
+    hybrid_mode
+    first_bin_centre
+
+    Returns
+    -------
+
+    """
+    low_side_multiplier = base ** (-1 / (2 * bands_per_division))
+    high_side_multiplier = base ** (1 / (2 * bands_per_division))
+
+    fft_bin_width = band[1] * 2 / nfft
+
+    # Start the frequencies list
+    bands_limits = []
+    bands_c = []
+    
+    # count the number of bands:
+    band_count = 0
+    center_freq = 0
+    if hybrid_mode:
+        bin_width = 0
+        while bin_width < fft_bin_width:
+            band_count = band_count + 1
+            center_freq = get_center_freq(base, bands_per_division, band_count, band[0])
+            bin_width = high_side_multiplier * center_freq - low_side_multiplier * center_freq
+
+        # now keep counting until the difference between the log spaced
+        # center frequency and new frequency is greater than .025
+        center_freq = get_center_freq(base, bands_per_division, band_count, band[0])
+        linear_bin_count = round(center_freq / fft_bin_width)
+        dc = abs(linear_bin_count * fft_bin_width - center_freq) + 0.1
+        while abs(linear_bin_count * fft_bin_width - center_freq) < dc:
+            # Compute next one
+            dc = abs(linear_bin_count * fft_bin_width - center_freq)
+            band_count = band_count + 1
+            linear_bin_count = linear_bin_count + 1
+            center_freq = get_center_freq(base, bands_per_division, band_count, band[0])
+
+        linear_bin_count = linear_bin_count - 1
+        band_count = band_count - 1
+
+        if (fft_bin_width * linear_bin_count) > band[1]:
+            linear_bin_count = band[1] / fft_bin_width + 1
+
+        for i in np.arange(linear_bin_count):
+            # Add the frequencies
+            fc = first_bin_centre + i * fft_bin_width
+            bands_c.append(fc)
+            bands_limits.append(fc - fft_bin_width / 2)
+
+    # count the log space frequencies
+    ls_freq = center_freq * high_side_multiplier
+    while ls_freq < band[1]:
+        fc = get_center_freq(base, bands_per_division, band_count, band[0])
+        ls_freq = fc * high_side_multiplier
+        bands_c.append(fc)
+        bands_limits.append(fc * low_side_multiplier)
+        band_count += 1
+    # Add the upper limit (bands_limits's length will be +1 compared to bands_c)
+    bands_limits.append(ls_freq)
+
+    return bands_limits, bands_c
+
+
+def get_center_freq(base, bands_per_division, n, firstOutBandcenterFreq):
+    if (bands_per_division == 10) or ((bands_per_division % 2) == 1):
+        center_freq = firstOutBandcenterFreq * base ** ((n - 1) / bands_per_division)
+    else:
+        b = bands_per_division * 0.3
+        center_freq = base * G ** ((2 * (n - 1) + 1) / (2 * b))
+
+    return center_freq
+
+
+def get_hybrid_millidecade_limits(band, nfft):
+    """
+
+    Parameters
+    ----------
+    band
+    nfft
+
+    Returns
+    -------
+
+    """
+    return get_bands_limits(band, nfft, base=10, bands_per_division=1000, hybrid_mode=True)
+
+
+def get_decidecade_limits(band, nfft):
+    """
+
+    Parameters
+    ----------
+    band
+    nfft
+
+    Returns
+    -------
+
+    """
+    return get_bands_limits(band, nfft, base=10, bands_per_division=10, hybrid_mode=False)
+
+
+def psd_ds_to_bands(psd, bands_limits, bands_c, fft_bin_width, method='spectrum'):
+    """
+    Group the psd according to the limits band_limits given. If a limit is not aligned with the limits in the psd
+    frequency axis then that psd frequency bin is divided in proportion to each of the adjacent bands. For more details
+    see publication Ocean Sound Analysis Software for Making Ambient Noise Trends Accessible (MANTA)
+    (https://doi.org/10.3389/fmars.2021.703650)
+
+    Parameters
+    ----------
+    psd: xarray DataArray
+        Output of pypam spectrum function
+    bands_c: list or array
+        Centre of the bands (used only of the ouput frequency axis naming)
+    bands_limits: list or array
+        Limits of the desired bands
+    method: string
+        Should be 'density' or 'spectrum'
+
+    Returns
+    -------
+    xarray DataArray with frequency_bins instead of frequency as a dimension.
+
+    """
+    fft_freq_lims = np.floor(np.array(bands_limits) / fft_bin_width) + fft_bin_width / 2
+
+    weights_diff = (fft_freq_lims - bands_limits) / fft_bin_width
+    psd_bands = psd.groupby_bins('frequency', bins=bands_limits, labels=bands_c).sum()
+    psd_limits = psd['band_spectrum'].sel(frequency=fft_freq_lims[:-1] + fft_bin_width / 2)
+    psd_diff = (psd_limits * weights_diff[:-1])
+    psd_bands['band_spectrum'].loc[1:] = psd_bands['band_spectrum'].sel(
+        frequency_bins=psd_bands.frequency_bins[1:]) - psd_diff.diff('frequency').values.T
+
+    if method == 'density':
+        bandwidths = psd.frequency.diff()
+        psd_bands = psd_bands / bandwidths
+
+    return psd_bands
+
+# Original MANTA code
+# def psd_to_bands(psd, bands_limits, bands_c):
+#     nRows = len(psd)
+#     bandsOut = np.zeros(nRows, lastBand-firstBand+1)
+#     step = fftBinSize / 2
+#     nFFTBins = size(psd, 2)
+#     startOffset = floor(bin1CenterFrequency / fftBinSize)
+#
+#     for row in np.arange(nRows):
+#         for j in np.arange(firstBand, lastBand):
+#             minFFTBin = np.floor((freqTable[j,1] / fftBinSize) + step) + 1 - startOffset
+#             maxFFTBin = np.floor((freqTable[j,3] / fftBinSize) + step) + 1 - startOffset
+#             if maxFFTBin > nFFTBins:
+#                 maxFFTBin = nFFTBins
+#
+#             if minFFTBin < 1:
+#                 minFFTBin = 1
+#
+#             if minFFTBin == maxFFTBin:
+#                 bandsOut[row, j] = psd[row, minFFTBin] *((freqTable[j, 3]- freqTable[j, 1])/ fftBinSize)
+#             else:
+#                 # Add the first partial FFT bin - take the top of the bin and
+#                 # subtract the lower freq to get the amount we will use:
+#                 # the top freq of a bin is bin# * step size - binSize/2 since bin
+#                 #
+#                 lowerFactor = ((minFFTBin - step) * fftBinSize - freqTable[j, 1])
+#                 bandsOut[row, j] = psd(row, minFFTBin) * lowerFactor
+#
+#                 # Add the last partial FFT bin.
+#                 upperFactor = freqTable[j, 3] - (maxFFTBin - 1.5*fftBinSize) * fftBinSize
+#                 bandsOut[row, j] = bandsOut[row, j] + psd(row, maxFFTBin)* upperFactor
+#
+#                 # Add any FFT bins in between min and max.
+#                 if (maxFFTBin - minFFTBin) > 1:
+#                     bandsOut[row, j] = bandsOut[row, j] + sum(psd[row, minFFTBin+1:maxFFTBin-1])
+#     return bandsOut
+
 def pcm2float(s, dtype='float64'):
     """
     Convert PCM signal to floating point with a range from -1 to 1.
