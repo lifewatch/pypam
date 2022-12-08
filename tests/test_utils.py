bf0c7f5ee9d51c840594a4a749d628aed507f1c1
Carlos Rueda <carueda@mbari.org>
2022-12-08 12:16:26 -0800

    make test runnable from root and with ability to disable the plots via env var

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 import pandas as pd
 import xarray
+from os import getenv
 import matplotlib.pyplot as plt
 import scipy
 
@@ -13,7 +14,7 @@ test_freqs = [400, fs / 4]
 samples = fs
 noise_amp = 100
 signal_amp = 100
-data = pd.read_csv('./test_data/signal_data.csv', header=None)[0].values
+data = pd.read_csv('tests/test_data/signal_data.csv', header=None)[0].values
 t = np.linspace(0, 1 - 1 / fs, samples)
 phase = 2 * np.pi * t
 for test_freq in test_freqs:
@@ -26,7 +27,7 @@ nfft = fs
 class TestMillidecades(unittest.TestCase):
     def test_get_millidecade_bands(self):
         bands_limits, bands_c = utils.get_hybrid_millidecade_limits(band=[0, fs/2], nfft=nfft)
-        mdec_bands_test = pd.read_csv('./test_data/mdec_bands_test.csv', header=None)
+        mdec_bands_test = pd.read_csv('tests/test_data/mdec_bands_test.csv', header=None)
         assert ((mdec_bands_test.iloc[:, 0] - bands_limits[:-1]) > 5e-5).sum() == 0
         assert ((mdec_bands_test.iloc[:, 2] - bands_limits[1:]) > 5e-5).sum() == 0
         assert ((mdec_bands_test.iloc[:, 1] - bands_c) > 5e-5).sum() == 0
@@ -45,11 +46,10 @@ class TestMillidecades(unittest.TestCase):
         fbands = scipy.fft.rfftfreq(nfft, 1/fs)
 
         # Load the spectrum used for MANTA
-        spectra_manta = pd.read_csv('./test_data/spectra.csv', header=None)
+        spectra_manta = pd.read_csv('tests/test_data/spectra.csv', header=None)
 
         # Check if they are the same
         assert (abs(spectra_manta[0].values - spectra) > 1e-5).sum() == 0
-        print('Spectrum is the same with a 1e-5 precision')
 
         # Convert the spectra to a datarray
         psd_da = xarray.DataArray([spectra], coords={'id': [0], 'frequency': fbands}, dims=['id', 'frequency'])
@@ -57,22 +57,22 @@ class TestMillidecades(unittest.TestCase):
         milli_psd = utils.psd_ds_to_bands(psd_da, bands_limits, bands_c, fft_bin_width=fs/nfft, db=False)
 
         # Read MANTA's output
-        mdec_power_test = pd.read_csv('./test_data/mdec_power_test.csv')
+        mdec_power_test = pd.read_csv('tests/test_data/mdec_power_test.csv')
 
-        # Plot the two outputs for comparison
-        fig, ax = plt.subplots()
-        ax.plot(milli_psd.frequency_bins, mdec_power_test['sum'], label='MANTA')
-        plt.legend()
-        plt.show()
+        if getenv('PYPAM_TEST_NO_PLOTS') is None:
+            # Plot the two outputs for comparison
+            fig, ax = plt.subplots()
+            ax.plot(milli_psd.frequency_bins, mdec_power_test['sum'], label='MANTA')
+            plt.legend()
+            plt.show()
 
-        fig, ax = plt.subplots()
-        milli_psd.plot(ax=ax, label='pypam')
-        plt.legend()
-        plt.show()
+            fig, ax = plt.subplots()
+            milli_psd.plot(ax=ax, label='pypam')
+            plt.legend()
+            plt.show()
 
         # Check if the results are the same
         assert ((mdec_power_test['sum'] - milli_psd.sel(id=0).values).abs() > 1e-5).sum() == 0
-        print('Results are the same with a 1e-5 precision')
 
 
 if __name__ == '__main__':
