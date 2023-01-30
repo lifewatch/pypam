3b06a448493f7dbec385f8b8c8a720e93ce9254f
Clea Parcerisas <cleaparcerisas@gmail.com>
2023-01-30 13:28:00 +0100

    changed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='lifewatch-pypam',
-    version='0.1.8',
+    version='0.1.9',
     description='Facilitate acoustic processing from underwater acoustic recorders',
     author='Clea Parcerisas',
     author_email='cleap@vliz.be',
