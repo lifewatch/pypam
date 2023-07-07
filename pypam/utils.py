a85baaa72a6441868b2b26813f9e99ed4cdb0b9a
Clea Parcerisas <cleaparcerisas@gmail.com>
2023-07-07 13:50:17 +0200

    changed the plots so the frequency dimension is inferred instead of using always the name 'frequency'

--- pypam/utils.py
+++ pypam/utils.py
@@ -553,8 +553,9 @@ def merge_ds(ds, new_ds, attrs_to_vars):
     return ds
 
 
-def compute_spd(psd_evolution, h=1.0, percentiles=None, max_val=None, min_val=None):
-    pxx = psd_evolution['band_density'].to_numpy().T
+def compute_spd(psd_evolution, data_var='band_density', h=1.0, percentiles=None, max_val=None, min_val=None):
+    pxx = psd_evolution[data_var].to_numpy().T
+    freq_axis = psd_evolution[data_var].dims[1]
     if percentiles is None:
         percentiles = []
     if min_val is None:
@@ -571,15 +572,15 @@ def compute_spd(psd_evolution, h=1.0, percentiles=None, max_val=None, min_val=No
         percentiles_names.append('L%s' % str(100 - level_p))
 
     spd_arr = xarray.DataArray(data=spd,
-                               coords={'frequency': psd_evolution.frequency, 'spl': bin_edges[:-1]},
-                               dims=['frequency', 'spl'])
+                               coords={freq_axis: psd_evolution[freq_axis].values, 'spl': bin_edges[:-1]},
+                               dims=[freq_axis, 'spl'])
     p_arr = xarray.DataArray(data=p.T,
-                             coords={'frequency': psd_evolution.frequency, 'percentiles': percentiles_names},
-                             dims=['frequency', 'percentiles'])
+                             coords={freq_axis: psd_evolution[freq_axis].values, 'percentiles': percentiles_names},
+                             dims=[freq_axis, 'percentiles'])
     spd_ds = xarray.Dataset(data_vars={'spd': spd_arr, 'value_percentiles': p_arr})
     units_attrs = output_units.get_units_attrs(method_name='spd', log=False)
     spd_ds['spd'].attrs.update(units_attrs)
-    spd_ds['spl'].attrs.update(psd_evolution['band_density'].attrs)
+    spd_ds['spl'].attrs.update(psd_evolution[data_var].attrs)
     return spd_ds
 
 
