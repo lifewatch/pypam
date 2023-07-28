d9ca3c8e269be28fb230b6bd616a83ca8b89f680
Simon Laurent <simon.laurent@vliz.be>
2023-07-28 14:15:12 +0200

    test: added a test for the function update_freq_cal from utils.py used in the wrappers in AcuFile and ASA classes

--- tests/test_acoustic_file.py
+++ tests/test_acoustic_file.py
@@ -2,14 +2,20 @@ import unittest
 from pypam.acoustic_file import AcuFile
 import pyhydrophone as pyhy
 from tests import skip_unless_with_plots
+import pathlib
+import matplotlib.pyplot as plt
 
+plt.rcParams.update(plt.rcParamsDefault)
 
 # Hydrophone Setup
 # If Vpp is 2.0 then it means the wav is -1 to 1 directly related to V
 model = 'ST300HF'
 name = 'SoundTrap'
 serial_number = 67416073
-soundtrap = pyhy.soundtrap.SoundTrap(name=name, model=model, serial_number=serial_number)
+calibration_file = pathlib.Path("tests/test_data/calibration_data.xlsx")
+soundtrap = pyhy.soundtrap.SoundTrap(name=name, model=model, serial_number=serial_number,
+                                     calibration_file=calibration_file, freq_col_id=1, sens_col_id=29,
+                                     start_data_id=6)
 
 
 class TestAcuFile(unittest.TestCase):
@@ -31,3 +37,9 @@ class TestAcuFile(unittest.TestCase):
     def test_nmf(self):
         self.acu_file.source_separation(window_time=1.0, n_sources=15,
                                         binsize=None, save_path=None, verbose=False, band=None)
+
+    def test_update_freq_cal(self):
+        ds_psd = self.acu_file.psd()
+        ds_psd_updated = self.acu_file.update_freq_cal(ds=ds_psd, data_var='band_density')
+        print(ds_psd['band_density'].values)
+        print(ds_psd_updated['band_density'].values)
