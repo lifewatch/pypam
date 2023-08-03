746df2d34111a50cc15a15d379e55386d91d4d4c
Simon Laurent <simon.laurent@vliz.be>
2023-08-03 16:11:45 +0200

    test: added assertions in the tests to check if the values are consistent

--- tests/test_acoustic_indices.py
+++ tests/test_acoustic_indices.py
@@ -3,6 +3,7 @@ from pypam.acoustic_file import AcuFile
 from pypam.signal import Signal
 import pypam.acoustic_indices
 import pyhydrophone as pyhy
+import numpy as np
 
 
 # Hydrophone Setup
@@ -17,32 +18,40 @@ acu_file = AcuFile(sfile='tests/test_data/67416073.210610033655.wav', hydrophone
 class TestAcousticIndices(unittest.TestCase):
     def setUp(self) -> None:
         self.signal = Signal(signal=acu_file.signal(units='upa'), fs=acu_file.fs, channel=acu_file.channel)
-        self.frequencies, _, self.spectrogram = self.signal.spectrogram()
-        self.spectrogram = 10**(self.spectrogram/20)
+        self.frequencies, _, self.spectrogram = self.signal.spectrogram(db=False)
 
     def test_compute_aci(self):
-        pypam.acoustic_indices.compute_aci(sxx=self.spectrogram)
+        aci = pypam.acoustic_indices.compute_aci(sxx=self.spectrogram)
+        assert aci >= 0
 
     def test_compute_bi(self):
-        pypam.acoustic_indices.compute_bi(sxx=self.spectrogram, frequencies=self.frequencies)
+        bi = pypam.acoustic_indices.compute_bi(sxx=self.spectrogram, frequencies=self.frequencies)
+        assert bi >= 0
 
     def test_compute_sh(self):
-        pypam.acoustic_indices.compute_sh(sxx=self.spectrogram)
+        sh = pypam.acoustic_indices.compute_sh(sxx=self.spectrogram)
+        assert np.logical_and(sh >= 0, sh <= 1)
 
     def test_compute_th(self):
-        pypam.acoustic_indices.compute_th(s=self.signal.signal)
+        th = pypam.acoustic_indices.compute_th(s=self.signal.signal)
+        assert np.logical_and(th >= 0, th <= 1)
 
     def test_compute_ndsi(self):
-        pypam.acoustic_indices.compute_ndsi(sxx=self.spectrogram, frequencies=self.frequencies)
+        ndsi = pypam.acoustic_indices.compute_ndsi(sxx=self.spectrogram, frequencies=self.frequencies)
+        assert np.logical_and(ndsi >= -1, ndsi <= 1)
 
     def test_compute_aei(self):
-        pypam.acoustic_indices.compute_aei(sxx=self.spectrogram, frequencies=self.frequencies)
+        aei = pypam.acoustic_indices.compute_aei(sxx=self.spectrogram, frequencies=self.frequencies)
+        assert np.logical_and(aei >= 0, aei <= 1)
 
     def test_compute_adi(self):
-        pypam.acoustic_indices.compute_adi(sxx=self.spectrogram, frequencies=self.frequencies)
+        adi = pypam.acoustic_indices.compute_adi(sxx=self.spectrogram, frequencies=self.frequencies)
+        assert adi >= 0
 
     def test_compute_zcr(self):
-        pypam.acoustic_indices.compute_zcr(s=self.signal.signal, fs=self.signal.fs)
+        zcr = pypam.acoustic_indices.compute_zcr(s=self.signal.signal, fs=self.signal.fs)
+        assert np.logical_and(zcr >= 0, zcr <= 1)
 
     def test_compute_zcr_avg(self):
-        pypam.acoustic_indices.compute_zcr_avg(s=self.signal.signal, fs=self.signal.fs)
+        zcr_avg = pypam.acoustic_indices.compute_zcr_avg(s=self.signal.signal, fs=self.signal.fs)
+        assert np.logical_and(zcr_avg >= 0, zcr_avg <= 1)
